2191efe97a7ed968650a0fbbe5cdb2380cc87b64
shaykotselim <shaykoteasital@gmail.com>
2025-05-07 00:35:20 +0600

    added even odd

---
+++ even_odd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main (){
+    int a;
+    std:: cout << "Enter Value of a: " ;
+    std::cin >> a;
+    if( a % 2 ==0){
+        std:: cout << "Even Number " << std:: endl;
+    }
+    else {
+        std:: cout << "Odd Number " << std:: endl;
+    }
+}

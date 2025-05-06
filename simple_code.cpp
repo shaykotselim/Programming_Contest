5bcc52b6f6fe22c73ef8a255299d9910c68051c0
shaykotselim <shaykoteasital@gmail.com>
2025-05-06 20:47:41 +0600

    added simple code

--- simple_code.cpp
+++ simple_code.cpp
@@ -1,10 +1,18 @@
-#include <iostream>;
+#include <iostream>
 
 using namespace std;
 
 int main (){
+    int a , b ,c;
+   cout << "Enter Value of a: " ;
+    cin >> a ;
+   cout << "Enter Value of b: " ;
+    cin >> b;
 
-    cout << "\n" << 6 + 2;
+    c = a + b;
+   
+    cout << "Total Value of C" << c << endl;
+   
 
     return 0;
 }
---
+++ simple_input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main (){
+    int a, b , c;
+    std:: cout << "Enter Value of a: ";
+
+    std:: cin >> a;
+    std:: cout << "Enter Value of b: ";
+    std:: cin >> b;
+
+    c = a + b;
+    std:: cout << "Total Value of C : "<< c << std:: endl;
+    return 0;
+}
